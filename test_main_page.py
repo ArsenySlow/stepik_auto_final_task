966fa1ecf80c1915489fcdebc796b2e4ee7f099d
ArsenySlow <116286402+ArsenySlow@users.noreply.github.com>
2023-05-12 14:44:59 +0400

    page object styles added

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,7 @@
-import time
-from selenium.webdriver.common.by import By
-
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    browser.set_window_size(1920, 1080)
-    browser.implicitly_wait(10)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(5)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
