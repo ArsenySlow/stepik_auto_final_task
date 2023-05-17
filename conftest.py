7e2e88c5f66fe91ee9675f64f7520b5867937272
ArsenySlow <116286402+ArsenySlow@users.noreply.github.com>
2023-05-17 18:57:21 +0400

    deleting 'browser = None'

--- conftest.py
+++ conftest.py
@@ -13,23 +13,20 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-
-    browser = None
     user_language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
         browser.implicitly_wait(10)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-
         browser = webdriver.Firefox()
         options = Ops()
         options.set_preference("intl.accept_languages", user_language)
-        driver = webdriver.Firefox(options=options)
+        webdriver.Firefox(options=options)
+        browser.implicitly_wait(10)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
